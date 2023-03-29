363cb7e9bca1462c9f76429b5d5fd5e574fea3c8
Diego Munoz <dimuva_25@hotmail.com>
2023-03-29 17:15:04 -0500

    Admin and Support models

---
+++ server/models/Support.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const supportSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  fullName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minlength: 6 },
+  phone: { type: String },
+  isActive: { type: Boolean, default: true },
+}, { timestamps: true });
+
+supportSchema.pre('save', async function(next) {
+  try {
+    if (!this.isModified('password')) {
+      return next();
+    }
+    const hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+supportSchema.methods.comparePassword = async function(candidatePassword) {
+  try {
+    const isMatch = await bcrypt.compare(candidatePassword, this.password);
+    return isMatch;
+  } catch (err) {
+    return false;
+  }
+};
+
+const Support = mongoose.model('Support', supportSchema);
+
+module.exports = Support;
