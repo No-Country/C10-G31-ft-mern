56cb5c6cb813792ca7ca74f325727c08d163298c
Jesus <jesusdavidgonzales1102@gmail.com>
2023-03-31 12:08:30 -0500

    Modification

--- client/src/pages/Shopping_cart.tsx
+++ client/src/pages/Shopping_cart.tsx
@@ -1,34 +1,13 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import Style from '../styles/Shopping_cart.module.css'
-import { FaBars, FaRegHeart, FaRegUserCircle, FaArrowLeft, FaRegDotCircle, FaCreditCard, FaRegTrashAlt} from 'react-icons/fa'
-import { HiOutlineShoppingCart } from 'react-icons/hi'
+import { FaArrowLeft, FaRegDotCircle, FaCreditCard, FaRegTrashAlt} from 'react-icons/fa'
+import Header from '../components/shared/Header'
 
 const Carrito = () => {
     return(
         <div className={Style.container_cart}>
-            <div className={Style.head_cart}>
-                <div className={Style.container_1}>
-                    <div className={Style.logo}>
-                        Logo
-                    </div>
-                    <div className={Style.container_icons}>
-                        <Link href=''>
-                            <FaRegUserCircle className={Style.icon_head_cart} />
-                        </Link>
-                        <Link href=''>
-                            <FaRegHeart className={Style.icon_head_cart} />
-                        </Link>
-                        <Link href=''>
-                            <HiOutlineShoppingCart className={Style.icon_head_cart} />
-                        </Link>
-                    </div>
-                </div>
-                    <div className={Style.container_2}>
-                    <FaBars className={Style.iconMenu} />
-                    <input type='text' className={Style.seeker} />
-                </div>
-            </div>
+            <Header />
             <div className={Style.back_cart}>
                 <Link href=''>
                     <FaArrowLeft className={Style.icon_back} />
