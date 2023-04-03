6212da3e351decdc6192017c14d54fcfda955dd7
Jesus <jesusdavidgonzales1102@gmail.com>
2023-04-02 22:36:55 -0500

    New page payment methods

---
+++ client/src/pages/Payment_method.tsx
@@ -0,0 +1,33 @@
+import Header from "@/components/shared/Headerspotech"
+import Style from '../styles/Payment_method.module.css'
+import { FaArrowLeft } from 'react-icons/fa'
+import Link from "next/link"
+
+const PaymentMethod = () => {
+    return(
+        <div className={Style.Container_payment}>
+            <Header />
+            <div className={Style.Methods_pay}>
+                <div className={Style.back_payment}>
+                    <Link href={'/'}>
+                        <FaArrowLeft />
+                    </Link>
+                    <p>Seleccionar metodo de pago</p>
+                </div>
+                <div className={Style.Container_methods}>
+                    <div className={Style.Method_pay}>
+                        <input type='radio' />
+                        <p>Tarjeta de credito/debito</p>
+                    </div>
+                    <div className={Style.Method_pay}>
+                        <input type='radio' />
+                        <p>Efectivo en el local</p>
+                    </div>
+                </div>
+                <button className={Style.buttonContinue}>Continuar compra</button>
+            </div>
+        </div>
+    )
+}
+
+export default PaymentMethod
