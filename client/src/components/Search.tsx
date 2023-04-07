e9a29e28323c19294e7c3cfcc5cf8dd4ca218de9
Elkin <elquin50000@gmail.com>
2023-04-07 11:50:56 -0500

    Burger Menu and Search

---
+++ client/src/components/Search.tsx
@@ -0,0 +1,57 @@
+import { AiOutlineHistory } from 'react-icons/ai';
+import { MdClose, MdOutlineKeyboardBackspace, MdOutlineArrowOutward } from 'react-icons/md';
+
+
+interface SearchProps {
+  searchActive: boolean;
+  setSearchActive: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+
+const Search = ({searchActive, setSearchActive}: SearchProps) => {
+  return (
+    <div className='bg-white fixed top-0 left-0 z-10 w-full min-h-screen'>
+      <div className='my-1'>
+        <div className='flex justify-between items-center border-b shadow-md'>
+          <div className='flex gap-3 items-center py-1 px-6'>
+              <MdOutlineKeyboardBackspace className='w-6 h-6 cursor-pointer' onClick={() => setSearchActive(!searchActive)} />
+              <input className='placeholder:text-gray-400 outline-none text-sm font-bold' placeholder='Motora G6 Plus' type='text' />
+          </div>
+          <MdClose className='w-6 h-6 mr-6' />
+        </div>
+        <div className='space-y-2 my-2 px-6'>
+          <div className='flex justify-between items-center'>
+              <div className='flex gap-4 items-center'>
+                  <AiOutlineHistory className='w-5 h-5 transform -scale-x-100' />
+                  <p className='font-bold text-sm'>MSI Katana</p>
+              </div>
+              <MdOutlineArrowOutward className='w-6 h-6 -rotate-90' />
+          </div>
+          <div className='flex justify-between items-center'>
+              <div className='flex gap-4 items-center'>
+                  <AiOutlineHistory className='w-5 h-5 transform -scale-x-100' />
+                  <p className='font-bold text-sm'>Lenovo Thinkpad L15 2° Gen</p>
+              </div>
+              <MdOutlineArrowOutward className='w-6 h-6 -rotate-90' />
+          </div>
+          <div className='flex justify-between items-center'>
+              <div className='flex gap-4 items-center'>
+                  <AiOutlineHistory className='w-5 h-5 transform -scale-x-100' />
+                  <p className='font-bold text-sm'>Cargador Universal Notebook</p>
+              </div>
+              <MdOutlineArrowOutward className='w-6 h-6 -rotate-90' />
+          </div>
+          <div className='flex justify-between items-center'>
+              <div className='flex gap-4 items-center'>
+                  <AiOutlineHistory className='w-5 h-5 transform -scale-x-100' />
+                  <p className='font-bold text-sm'>SSD M.2 Firecuda</p>
+              </div>
+              <MdOutlineArrowOutward className='w-6 h-6 -rotate-90' />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Search
--- client/src/components/shared/Header.tsx
+++ client/src/components/shared/Header.tsx
@@ -2,31 +2,46 @@ import Link from 'next/link'
 import Style from '../../styles/Header.module.css'
 import { FaBars, FaRegHeart, FaRegUserCircle, } from 'react-icons/fa'
 import { HiOutlineShoppingCart } from 'react-icons/hi'
+import { useState } from 'react'
+import BurgerMenu from '../BurgerMenu'
+import Search from '../Search'
 
 const Header = () => {
+
+    const [ menuActive, setMenuActive ] = useState(false)
+    const [ searchActive, setSearchActive ] = useState(false)
+
     return(
-        <div className={Style.head_cart}>
-            <div className={Style.container_1}>
-                <div className={Style.logo}>
-                    <Link href='/'>Logo</Link>
+        <>
+            {searchActive && (
+                <Search searchActive={searchActive} setSearchActive={setSearchActive} />
+            )}
+            {menuActive && (
+                <BurgerMenu menuActive={menuActive} setMenuActive={setMenuActive} />
+            )}
+            <div className={Style.head_cart}>
+                <div className={Style.container_1}>
+                    <div className={Style.logo}>
+                        <Link href='/'>Logo</Link>
+                    </div>
+                    <div className={Style.container_icons}>
+                        <Link href=''>
+                            <FaRegUserCircle className={Style.icon_head_cart} />
+                        </Link>
+                        <Link href=''>
+                            <FaRegHeart className={Style.icon_head_cart} />
+                        </Link>
+                        <Link href='/ShoppingCart'>
+                            <HiOutlineShoppingCart className={Style.icon_head_cart} />
+                        </Link>
+                    </div>
                 </div>
-                <div className={Style.container_icons}>
-                    <Link href=''>
-                        <FaRegUserCircle className={Style.icon_head_cart} />
-                    </Link>
-                    <Link href=''>
-                        <FaRegHeart className={Style.icon_head_cart} />
-                    </Link>
-                    <Link href='/ShoppingCart'>
-                        <HiOutlineShoppingCart className={Style.icon_head_cart} />
-                    </Link>
+                    <div className={Style.container_2}>
+                    <FaBars className={Style.iconMenu} onClick={() => setMenuActive(!menuActive)} />
+                    <input type='text' onClick={() => setSearchActive(!searchActive)} className={Style.seeker} />
                 </div>
             </div>
-                <div className={Style.container_2}>
-                <FaBars className={Style.iconMenu} />
-                <input type='text' className={Style.seeker} />
-            </div>
-        </div>
+        </>
     )
 }
 
