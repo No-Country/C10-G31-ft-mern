444a8b38eb379381d81c74cf1a51a9f1ba35c607
Jesus <jesusdavidgonzales1102@gmail.com>
2023-04-11 17:44:28 -0500

    Component responsive tablet

--- client/src/pages/ListResults.tsx
+++ client/src/pages/ListResults.tsx
@@ -8,6 +8,7 @@ const ListProducts = () => {
         <div>
             <Header />
             <div className={Style.containerResults}>
+                <p className={Style.Category}>Home &gt; Smartphones &gt; Android &gt; Motorola</p>
                 <div className={Style.cantResult}>
                     <p>233 resultados</p>
                     <span></span>
@@ -16,8 +17,10 @@ const ListProducts = () => {
                     <div className={Style.result}>
                         <Image className={Style.imageResult} src={''} alt={''}  />
                         <div className={Style.infoResult}>
+                            <p className={Style.brandResult}>Marca producto</p>
                             <p className={Style.nameResult}>Nombre Producto</p>
                             <p className={Style.priceResult}>$98670,02</p>
+                            <p className={Style.shippingcostResult}>Envio gratis</p>
                             <div className={Style.icons}>
                                 <FaRegHeart />
                                 <FaShareAlt />
@@ -27,8 +30,10 @@ const ListProducts = () => {
                     <div className={Style.result}>
                         <Image className={Style.imageResult} src={''} alt={''}  />
                         <div className={Style.infoResult}>
-                            <p>Nombre Producto</p>
+                            <p className={Style.brandResult}>Marca producto</p>
+                            <p className={Style.nameResult}>Nombre Producto</p>
                             <p className={Style.priceResult}>$98670,02</p>
+                            <p className={Style.shippingcostResult}>Envio gratis</p>
                             <div className={Style.icons}>
                                 <FaRegHeart />
                                 <FaShareAlt />
@@ -38,8 +43,10 @@ const ListProducts = () => {
                     <div className={Style.result}>
                         <Image className={Style.imageResult} src={''} alt={''}  />
                         <div className={Style.infoResult}>
-                            <p>Nombre Producto</p>
+                            <p className={Style.brandResult}>Marca producto</p>
+                            <p className={Style.nameResult}>Nombre Producto</p>
                             <p className={Style.priceResult}>$98670,02</p>
+                            <p className={Style.shippingcostResult}>Envio gratis</p>
                             <div className={Style.icons}>
                                 <FaRegHeart />
                                 <FaShareAlt />
@@ -49,8 +56,10 @@ const ListProducts = () => {
                     <div className={Style.result}>
                         <Image className={Style.imageResult} src={''} alt={''}  />
                         <div className={Style.infoResult}>
-                            <p>Nombre Producto</p>
+                            <p className={Style.brandResult}>Marca producto</p>
+                            <p className={Style.nameResult}>Nombre Producto</p>
                             <p className={Style.priceResult}>$98670,02</p>
+                            <p className={Style.shippingcostResult}>Envio gratis</p>
                             <div className={Style.icons}>
                                 <FaRegHeart />
                                 <FaShareAlt />
@@ -60,8 +69,62 @@ const ListProducts = () => {
                     <div className={Style.result}>
                         <Image className={Style.imageResult} src={''} alt={''}  />
                         <div className={Style.infoResult}>
-                            <p>Nombre Producto</p>
+                            <p className={Style.brandResult}>Marca producto</p>
+                            <p className={Style.nameResult}>Nombre Producto</p>
                             <p className={Style.priceResult}>$98670,02</p>
+                            <p className={Style.shippingcostResult}>Envio gratis</p>
+                            <div className={Style.icons}>
+                                <FaRegHeart />
+                                <FaShareAlt />
+                            </div>
+                        </div>
+                    </div>
+                    <div className={Style.result}>
+                        <Image className={Style.imageResult} src={''} alt={''}  />
+                        <div className={Style.infoResult}>
+                            <p className={Style.brandResult}>Marca producto</p>
+                            <p className={Style.nameResult}>Nombre Producto</p>
+                            <p className={Style.priceResult}>$98670,02</p>
+                            <p className={Style.shippingcostResult}>Envio gratis</p>
+                            <div className={Style.icons}>
+                                <FaRegHeart />
+                                <FaShareAlt />
+                            </div>
+                        </div>
+                    </div>
+                    <div className={Style.result}>
+                        <Image className={Style.imageResult} src={''} alt={''}  />
+                        <div className={Style.infoResult}>
+                            <p className={Style.brandResult}>Marca producto</p>
+                            <p className={Style.nameResult}>Nombre Producto</p>
+                            <p className={Style.priceResult}>$98670,02</p>
+                            <p className={Style.shippingcostResult}>Envio gratis</p>
+                            <div className={Style.icons}>
+                                <FaRegHeart />
+                                <FaShareAlt />
+                            </div>
+                        </div>
+                    </div>
+                    <div className={Style.result}>
+                        <Image className={Style.imageResult} src={''} alt={''}  />
+                        <div className={Style.infoResult}>
+                            <p className={Style.brandResult}>Marca producto</p>
+                            <p className={Style.nameResult}>Nombre Producto</p>
+                            <p className={Style.priceResult}>$98670,02</p>
+                            <p className={Style.shippingcostResult}>Envio gratis</p>
+                            <div className={Style.icons}>
+                                <FaRegHeart />
+                                <FaShareAlt />
+                            </div>
+                        </div>
+                    </div>
+                    <div className={Style.result}>
+                        <Image className={Style.imageResult} src={''} alt={''}  />
+                        <div className={Style.infoResult}>
+                            <p className={Style.brandResult}>Marca producto</p>
+                            <p className={Style.nameResult}>Nombre Producto</p>
+                            <p className={Style.priceResult}>$98670,02</p>
+                            <p className={Style.shippingcostResult}>Envio gratis</p>
                             <div className={Style.icons}>
                                 <FaRegHeart />
                                 <FaShareAlt />
