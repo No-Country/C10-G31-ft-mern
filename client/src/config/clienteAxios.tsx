6bf621206ed82dd43b72f4cdc471d4122ba75d03
Elkin <elquin50000@gmail.com>
2023-04-22 01:39:13 -0500

    Final Version

--- client/src/config/clienteAxios.tsx
+++ client/src/config/clienteAxios.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const clienteAxios = axios.create({
     // TODO Añadir a variables de entorno
-    baseURL: `http://localhost:4000/api/v1`
+    baseURL: `https://spotech-api-production.up.railway.app/api/v1`
 })
 
 export default clienteAxios
